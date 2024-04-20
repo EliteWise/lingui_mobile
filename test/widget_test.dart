73f87e3128a5fbbbec1f9fa6c1d192f628c66e46
Florent <eliteman.fm@gmail.com>
2024-04-20 17:13:43 +0200

    Implement simple chat page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:lingui_mobile/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LinguiMobile());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
