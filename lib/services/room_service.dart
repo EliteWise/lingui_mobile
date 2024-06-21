fcc9cb6344e4d1d577d8fd91a1d241122c281f94
Florent <eliteman.fm@gmail.com>
2024-06-21 19:45:57 +0200

    Add message & CustomPainter widgets

--- lib/services/room_service.dart
+++ lib/services/room_service.dart
@@ -1,18 +1,27 @@
 // room_service.dart
 import 'dart:convert';
+import '../models/discussion.dart';
 import 'package:http/http.dart' as http;
-import 'package:lingui_mobile/models/discussion.dart';
 
 Future<String> createRoom(Discussion discussion) async {
-  final response = await http.post(Uri.parse('http://10.0.2.2:8080/createRoom'),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(discussion)
-  );
-  if (response.statusCode == 200) {
-    return response.body;
-  } else {
-    throw Exception('Failed to create room');
+  try {
+    final response = await http.post(
+      Uri.parse('http://10.0.2.2:8080/createRoom'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(discussion),
+    );
+
+    if (response.statusCode == 200) {
+      final responseJson = jsonDecode(response.body);
+      return responseJson['id'];
+    } else {
+      print('Failed to create room: ${response.statusCode}');
+      throw Exception('Failed to create room');
+    }
+  } catch (e) {
+    print('Failed to create room: $e');
+    throw Exception('Failed to create room: $e');
   }
 }
