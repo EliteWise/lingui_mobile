2e7214bcd1dbe997bcd45ee386ec8d05d4a66f3c
Florent <eliteman.fm@gmail.com>
2024-07-09 16:58:04 +0200

    Switch image gallery page to Stateful + add appbar icons + implement image adding images logic with image picker

--- lib/views/image_gallery_page.dart
+++ lib/views/image_gallery_page.dart
@@ -1,42 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
-class ImageGalleryPage extends StatelessWidget {
+class ImageGalleryPage extends StatefulWidget {
   const ImageGalleryPage({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => _ImageGalleryPageState();
+
+}
+
+class _ImageGalleryPageState extends State<ImageGalleryPage> {
+
+  List<String> listOfUrls = [
+    "https://via.placeholder.com/600x400.png?text=Image+1",
+    "https://via.placeholder.com/600x400.png?text=Image+1",
+    "https://via.placeholder.com/600x400.png?text=Image+1",
+    "https://upload.wikimedia.org/wikipedia/commons/7/77/Big_Nature_%28155420955%29.jpeg",
+    "https://upload.wikimedia.org/wikipedia/commons/7/77/Big_Nature_%28155420955%29.jpeg",
+    "https://upload.wikimedia.org/wikipedia/commons/7/77/Big_Nature_%28155420955%29.jpeg",
+    "https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg",
+    "https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg",
+    "https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg",
+    "https://w0.peakpx.com/wallpaper/265/481/HD-wallpaper-nature.jpg",
+    "https://w0.peakpx.com/wallpaper/265/481/HD-wallpaper-nature.jpg",
+    "https://w0.peakpx.com/wallpaper/265/481/HD-wallpaper-nature.jpg",
+    "https://e0.pxfuel.com/wallpapers/163/906/desktop-wallpaper-beautiful-nature-with-girl-beautiful-girl-with-nature-and-moon-high-resolution-beautiful.jpg",
+    "https://e0.pxfuel.com/wallpapers/163/906/desktop-wallpaper-beautiful-nature-with-girl-beautiful-girl-with-nature-and-moon-high-resolution-beautiful.jpg",
+    "https://e0.pxfuel.com/wallpapers/163/906/desktop-wallpaper-beautiful-nature-with-girl-beautiful-girl-with-nature-and-moon-high-resolution-beautiful.jpg"
+  ];
+
+  final ImagePicker picker = ImagePicker();
+
+  Future<void> _addImage() async {
+    final List<XFile>? images = await picker.pickMultiImage(limit: 4);
+    if(images != null) {
+      setState(() {
+        for(var image in images) {
+          listOfUrls.add("https://via.placeholder.com/600x400.png?text=Image+1"); // image.path
+        }
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    List<String> listOfUrls = [
-      "https://via.placeholder.com/600x400.png?text=Image+1",
-      "https://via.placeholder.com/600x400.png?text=Image+1",
-      "https://via.placeholder.com/600x400.png?text=Image+1",
-      "https://upload.wikimedia.org/wikipedia/commons/7/77/Big_Nature_%28155420955%29.jpeg",
-      "https://upload.wikimedia.org/wikipedia/commons/7/77/Big_Nature_%28155420955%29.jpeg",
-      "https://upload.wikimedia.org/wikipedia/commons/7/77/Big_Nature_%28155420955%29.jpeg",
-      "https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg",
-      "https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg",
-      "https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg",
-      "https://w0.peakpx.com/wallpaper/265/481/HD-wallpaper-nature.jpg",
-      "https://w0.peakpx.com/wallpaper/265/481/HD-wallpaper-nature.jpg",
-      "https://w0.peakpx.com/wallpaper/265/481/HD-wallpaper-nature.jpg",
-      "https://e0.pxfuel.com/wallpapers/163/906/desktop-wallpaper-beautiful-nature-with-girl-beautiful-girl-with-nature-and-moon-high-resolution-beautiful.jpg",
-      "https://e0.pxfuel.com/wallpapers/163/906/desktop-wallpaper-beautiful-nature-with-girl-beautiful-girl-with-nature-and-moon-high-resolution-beautiful.jpg",
-      "https://e0.pxfuel.com/wallpapers/163/906/desktop-wallpaper-beautiful-nature-with-girl-beautiful-girl-with-nature-and-moon-high-resolution-beautiful.jpg"
-    ];
+
+    final ImagePicker picker = ImagePicker();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Gallery"),
+        actions: [
+          Padding(
+              padding: EdgeInsets.only(right: 18),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.tune),
+                    onPressed: () {
+
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.image_outlined),
+                    onPressed: () {
+
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: _addImage,
+                  )
+                ],
+              )
+          )
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 3,
-            crossAxisSpacing: 8.0,
-            mainAxisSpacing: 8.0,
+            crossAxisSpacing: 12.0,
+            mainAxisSpacing: 12.0,
           ),
           itemCount: listOfUrls.length,
           itemBuilder: (context, index) {
