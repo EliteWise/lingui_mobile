1723448e5777946a8233ce58975c37a9bc39d712
Florent <eliteman.fm@gmail.com>
2024-09-07 18:58:49 +0200

    Setup firebase + onesginal to send/receive notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:lingui_mobile/firebase_options.dart';
 import 'package:lingui_mobile/views/community_page.dart';
 import 'package:lingui_mobile/views/country_folder_page.dart';
 import 'package:lingui_mobile/views/discussion_page.dart';
@@ -8,8 +10,17 @@ import 'package:lingui_mobile/views/login_page.dart';
 import 'package:lingui_mobile/views/navigation_page.dart';
 import 'package:lingui_mobile/views/profile_page.dart';
 import 'package:lingui_mobile/views/settings_page.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize("bc2db3a0-aac8-4457-9897-d763cd94ae4d");
+  OneSignal.Notifications.requestPermission(true);
+
   runApp(const LinguiMobile());
 }
 
@@ -24,7 +35,7 @@ class LinguiMobile extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const SettingsPage(),
+      home: const LoginPage(),
     );
   }
 
--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lingui_mobile/views/login_email_page.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 import 'discussion_page.dart';
 
