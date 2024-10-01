11bdd5d5e5e482429a38f6b0254ff19c88e6d27f
Florent <eliteman.fm@gmail.com>
2024-10-01 16:16:29 +0200

    Remove temp firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:lingui_mobile/firebase_options.dart';
 import 'package:lingui_mobile/views/country_folder_page.dart';
 import 'package:lingui_mobile/views/login_page.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
@@ -9,13 +8,7 @@ import 'dart:io' show Platform;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if(Platform.isAndroid) {
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-    OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
-    OneSignal.initialize("bc2db3a0-aac8-4457-9897-d763cd94ae4d");
-    OneSignal.Notifications.requestPermission(true);
-  }
 
   runApp(const LinguiMobile());
 }
